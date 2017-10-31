59f31f069fa59fbf658be99d3e0e1ba1883ea6f8
icehouze <icehouze@gmail.com>
2017-10-31 12:28:50 -0400

    Lesson 9: Route Model Binding

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,10 +14,8 @@ class TasksController extends Controller
 	    return view('tasks.index', compact('tasks'));
     }
 
-    public function show($id)
+    public function show(Task $task) // route model binding replaces: $task = Task::find($wildcard);
     {
-		$task = Task::find($id);
-
 	    return view('tasks.show', compact('task'));
     }
 }
