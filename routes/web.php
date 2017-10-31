b6df2589ec6f9e372574f5d6edd13ac8d724f8ed
icehouze <icehouze@gmail.com>
2017-10-31 13:53:26 -0400

    Lesson 10: Layouts and Structure

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,6 @@
 
 Route::get('/tasks', 'TasksController@index'); // ControllerName@method
 Route::get('/tasks/{task}', 'TasksController@show'); // ControllerName@method
+
+Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
