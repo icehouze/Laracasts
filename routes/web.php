3a9fdf5b0cba650b6cfcd1047eba171c518730c5
icehouze <icehouze@gmail.com>
2017-10-31 12:21:50 -0400

    Lesson 8: Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,4 @@
 <?php
 
-// import class
-use App\Task;
-
-Route::get('/tasks', function () {
-	// Laravel's query builder
-	// $tasks = DB::table('tasks')->latest()->get();
-
-	// Using Eloquent and a dedicated class
-	$tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-	// Laravel's query builder
-	// $task = DB::table('tasks')->find($id);
-
-	// Using Eloquent and a dedicated class
-	$task = Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index'); // ControllerName@method
+Route::get('/tasks/{task}', 'TasksController@show'); // ControllerName@method
