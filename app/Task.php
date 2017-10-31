4402ea33f9a4b271b39d1ba72eb71a20de912087
icehouze <icehouze@gmail.com>
2017-10-31 12:05:25 -0400

    Lesson 7: Eloquent 101

---
+++ app/Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //  Query scopes
+    public function scopeIncomplete($query) // Task::incomplete()
+    {
+    	return $query->where('completed', 0);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// import class
+use App\Task;
 
 Route::get('/tasks', function () {
 	// Laravel's query builder
-	$tasks = DB::table('tasks')->latest()->get();
+	// $tasks = DB::table('tasks')->latest()->get();
+
+	// Using Eloquent and a dedicated class
+	$tasks = Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-	$task = DB::table('tasks')->find($id);
+	// Laravel's query builder
+	// $task = DB::table('tasks')->find($id);
+
+	// Using Eloquent and a dedicated class
+	$task = Task::find($id);
 
     return view('tasks.show', compact('task'));
 });
